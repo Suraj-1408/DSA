42472ef07f7b90505e807b21f2ff51529a1c4825
Suraj Manoj Paswan <surajpaswanc0497@gmail.com>
2025-03-28 08:07:52 +0530

    returning index of searched target ,in rotated sorted array, handling edge cases.

--- BinarySearch/searchInRotatedSortedArray.java
+++ BinarySearch/searchInRotatedSortedArray.java
@@ -1,41 +1,11 @@
 class searchInRotatedSortedArray{
 
 	//method to search element in rotated sorted array accordingly return a boolean value.
-	public static boolean search(int[] nums, int target) {
+	public static int search(int[] nums, int target) {
         
-    /*    
-        //using binary search as the array is sorted.
-        //making use of two index start and end.
-        //computing mid and comparing it target, if target > mid , search right half ,else left half.
-        
-        Arrays.sort(nums);
-
-        int start = 0;
-        int end = nums.length-1;
-
-        while(start <= end){
-            int mid = start + (end-start)/2;
-
-            if(nums[mid] == target){
-                return true;
-            }
-
-            else if(nums[mid] > target){
-                //search in left half.
-                end = mid-1;
-            }
-
-            else{
-                start = mid+1;
-            }
-        }
-
-        return false;
-    */
-
-    //Second solution -  more fast and optimised one.
+    //APPROACH - Compare mid index value with target & array boundaries,(low or high index value.)
+    
     //Due to array rotation, the elements are divided into two different sorted interval.
-    //we need to find out that first for searching target in given array.
     //In order to ensure that our one of the interval sorted or not we need to compare
     //start index value with mid index value if(nums[start] <= nums[mid]) this mean left half is sorted.
     //else right half is sorted where nums[mid] <= nums[end]
@@ -49,33 +19,42 @@ class searchInRotatedSortedArray{
         //compute mid 
         int mid = start + (end-start)/2;
 
+	//if target is found at mid index.
         if(nums[mid] == target){
-            	System.out.println("Element found at Index:"+mid);
-		return true;
+            	System.out.println("Element "+nums[mid]+" found");
+		return mid;
         }
 
+	/*
         else if(nums[start] == nums[mid] && nums[mid] == nums[end]){
             start = start+1;
             end = end-1;
             continue;
         }
+	*/
 
-        else if(nums[start] <= nums[mid]){
-            //ensuring target lies in  between.
-            if(nums[start] <= target && target <= nums[mid]){
+	//NOW , SINCE MID INDEX IS NOT TARGET.
+	//NEXT,IDENTIFY WHICH HALF IS SORTED, 
+	//IF PARTICULAR HALF IS SORTED CHECK IF TARGET LIES IN THAT HALF.
+	
+        else if(nums[start] < nums[mid]){
+            
+	    //checking if target lies in this half or not.
+            if(nums[start] <= target && target < nums[mid]){
                 //if above condition is true , DISCARD THE RIGHT HALF AND SEARCH IN LEFT HALF.
                 end =  mid-1;
             }
-
+	
+	    //SEARCH IN RIGHT HALF. by updating start index to mid
             else{
-                start = mid+1;
+                start = mid;
             }
         }
 
         else{
 
             //ensure target lies in betweeen right half.
-            if(nums[mid] <= target && target <= nums[end]){
+            if(nums[mid] <= target && target < nums[end]){
                 start = mid+1;
             }
 
@@ -85,7 +64,7 @@ class searchInRotatedSortedArray{
         }
     }
 
-    return false;
+    return -1;
 
     }
 
@@ -93,7 +72,8 @@ class searchInRotatedSortedArray{
     //main()
     public static void main(String[] args){
     	
-    	int[] nums = {2,5,6,0,0,1,2};
+    	//int[] nums = {2,5,6,0,0,1,2};
+	int[] nums = {4,5,6,7,0,1,2};
 	int target = 3;
 	
 	System.out.println("Array ELements are:");
@@ -103,12 +83,12 @@ class searchInRotatedSortedArray{
 	System.out.println("");
 
 
-	boolean res = search(nums,target);
-	if(res){
-		System.out.println("Result:true");
+	int res = search(nums,target);
+	if(res != -1){
+		System.out.println("Element found at index "+res);
     	}	
 	else{
-		System.out.println("Element not found, Result:false");
+		System.out.println("Element "+target+" not found, Result:"+res);
 	}
 
 	}
